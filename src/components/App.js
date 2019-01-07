e6ad1d9ce0a55ef9268bd76777e76c29cdf35919
Tomasz Olejniczak <snustor@gmail.com>
2019-01-07 18:22:51 +0100

    Project files addition

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import style from './App.css';
 import Title from './Title';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
     constructor(props){
@@ -49,4 +50,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
